e2e3b154f1c8e3656e020504b8c37aad334e172f
ABDOALRG <mhmedayk@gmail.com>
2025-04-30 03:11:05 +0300

    Add files via upload

--- server (1).js
+++ server (1).js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.BOT_TOKEN;
-const id = process.env.OWNER_ID;
+const token = 'توكن'
+const id = 'ايدي حسابك'
 const address = 'https://www.google.com'
 
 const app = express();
